1d28f8d03d0655b0950e097cc689d12f0f746ee9
Lora <larachka.06@mail.ru>
2024-11-27 20:50:35 +0100

    Задача 2

--- eighth_py.py
+++ eighth_py.py
@@ -16,40 +16,72 @@
 попадания в тот или иной диапазон. Использовать обработку
 исключений.
 """
-import math
+# import math
+#
+# def calculator_imt(weight, height):
+#     return weight / math.pow(height, 2)
+#
+# def title_imt(imt):
+#     if imt < 18:
+#         return "Дефицит массы тела"
+#     elif 18 <= imt < 24.9:
+#         return "Нормальная масса тела"
+#     elif 25 <= imt < 29.9:
+#         return "Избыточная масса тела (предожирение)"
+#     else:
+#         return "Ожирение"
+#
+# try:
+#     weight = input("Введите свой вес в граммах: ")
+#     height = input("Введите свой рост в сантиметрах: ")
+#
+#     weight_kg = float(weight) / 1000
+#     height_m = float(height) / 100
+#
+#     if weight_kg <= 0 or height_m <= 0:
+#         raise ValueError("Вес и/или рост некорректно введены")
+#
+#     mass = calculator_imt(weight_kg, height_m)
+#     category = title_imt(mass)
+#
+#     print(f"Ваш индекс массы тела составляет: {mass:0.2f}, что соответствует: {category}")
+#
+# except ValueError as e:
+#     print(f"Ошибка ввода данных: {e}")
+# except Exception as e:
+#     print(f"Другая ошибка: {e}")
 
-def calculator_imt(weight, height):
-    return weight / math.pow(height, 2)
-
-def title_imt(imt):
-    if imt < 18:
-        return "Дефицит массы тела"
-    elif 18 <= imt < 24.9:
-        return "Нормальная масса тела"
-    elif 25 <= imt < 29.9:
-        return "Избыточная масса тела (предожирение)"
-    else:
-        return "Ожирение"
+"""
+2. Реализовать программу с функционалом калькулятора
+для операций над двумя числами. Числа и операция вводятся
+пользователем с клавиатуры. Использовать обработку
+исключений.
+"""
+def calc(a, b, operation):
+    if operation == "1":
+        return int(a) + int(b)
+    elif operation == "2":
+        return int(a) - int(b)
+    elif operation == "3":
+        return int(a) * int(b)
+    elif operation == "4":
+        return int(a) / int(b)
 
 try:
-    weight = input("Введите свой вес в граммах: ")
-    height = input("Введите свой рост в сантиметрах: ")
+    a = input("Введите первое число: ")
+    b = input("Введите второе число: ")
+    c = input("Выберите операцию : 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление ")
 
-    weight_kg = float(weight) / 1000
-    height_m = float(height) / 100
+    if b == 0 and c == "4":
+        raise ZeroDivisionError("На нуль делить нельзя")
 
-    if weight_kg <= 0 or height_m <= 0:
-        raise ValueError("Вес и/или рост некорректно введены")
+    result = calc(a, b, c)
 
-    mass = calculator_imt(weight_kg, height_m)
-    category = title_imt(mass)
-
-    print(f"Ваш индекс массы тела составляет: {mass:0.2f}, что соответствует: {category}")
+    print(result)
 
 except ValueError as e:
     print(f"Ошибка ввода данных: {e}")
+except ZeroDivisionError:
+    print("Ошибка: На нуль делить нельзя")
 except Exception as e:
     print(f"Другая ошибка: {e}")
-
-
-
